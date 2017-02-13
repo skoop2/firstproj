5bb80c94c2886045caba9f6f1af7291a8805de43
dstoev <dstoev@PC17142195>
2017-02-13 12:49:45 +0100

    scan

--- mixed_app/mixed_app/firstProjFun.h
+++ mixed_app/mixed_app/firstProjFun.h
@@ -3,7 +3,27 @@
 
 int fScan() {
 
+	int dpi = 0, farbtiefe=0; 
+	float breite = 0, hoehe = 0;
+	double groesse = 0;
 
+	printf("Bitte geben Sie die DPI ein: ");
+	scanf("%i", &dpi);
+
+	printf("Bitte geben Sie die Farbtiefe ein: ");
+	scanf("%i", &farbtiefe);
+
+	printf("Bitte geben Sie die Breite ein: ");
+	scanf("%i", &breite);
+
+	printf("Bitte geben Sie die Hoehe ein: ");
+	scanf("%i", &hoehe);
+
+	printf("++++++++++++++Ausgabe+++++++++++\n");
+
+	groesse = ((((breite*72) / 2.54) * (hoehe * 72) / 2.54) * farbtiefe) / 8 / 1024 / 1024;  
+
+	printf("Die zu erwartende Dateigr\x94\xE1\e entspricht %.4lf MiB\n", groesse);
 
 	return 0;
 }
